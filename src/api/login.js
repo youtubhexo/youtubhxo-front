ab119e2c3d79fac0fd3a6f15f103595bfbf424d3
desperado <youtubhexo@gmail.com>
2025-02-21 14:36:12 +0800

    first commit

---
+++ src/api/login.js
@@ -0,0 +1,54 @@
+import axios from '@/utils/request'
+
+/**
+ * 获取验证码接口
+ * @param {*} sid 唯一标识
+ */
+const getCode = (sid) => {
+  // axios.request({
+  //  method: 'get',
+  //  url: '/getCaptcha'
+  // })
+  return axios.get('/public/getCaptcha', {
+    params: {
+      sid: sid
+    }
+  })
+}
+
+/**
+ * 找回密码接口
+ * @param {} option 用户信息（邮箱，验证码）
+ */
+const forget = (option) => {
+  return axios.post('/forget', {
+    ...option
+  })
+}
+
+/**
+ * 登录接口
+ * @param {*} loginInfo 用户登录信息
+ */
+const login = (loginInfo) => {
+  return axios.post('/login/login', {
+    ...loginInfo
+  })
+}
+
+/**
+ * 注册接口
+ * @param {*} regInfo 用户注册信息
+ */
+const reg = (regInfo) => {
+  return axios.post('/login/reg', {
+    ...regInfo
+  })
+}
+
+export {
+  getCode,
+  forget,
+  login,
+  reg
+}
---
+++ src/config/index.js
@@ -0,0 +1,6 @@
+export default {
+  baseUrl: {
+    dev: 'http://localhost:36742',
+    pro: 'http://www.toimc.com:12000'
+  }
+}
